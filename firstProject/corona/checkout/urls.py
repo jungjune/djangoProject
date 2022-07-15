007db818c785f4cf7e4584980e8125ec1fad74b2
home <jungjune276@gmail.com>
2022-07-15 22:20:22 +0900

    up

--- firstProject/corona/checkout/urls.py
+++ firstProject/corona/checkout/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns =[
     path('', views.index, name='index'),
     path('second/',views.second,name='second'),
+    path('proje/',views.cafe, name='cafe'),
 ]
--- firstProject/corona/checkout/views.py
+++ firstProject/corona/checkout/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'checkout/index.html')
+
+def second(request):
+    pass
+
+def cafe(request):
+    return render(request, 'checkout/projemp.html')
